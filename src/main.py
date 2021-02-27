9dacef0876a27d9f3dc3269308fecd199d062f48
Thúlio Carvalho <thulioicc@gmail.com>
2021-02-27 14:35:34 -0300

    initial structure

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+TOKEN = os.environ['WEREWOLF_TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Efetuamos o login como {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('Hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
